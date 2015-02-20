6d4f3d4dbb5dda38bc7f8663c6f0f0bcc5fa7bff
Marcel Timmerman <mt1957@gmail.com>
2015-02-20 15:24:51 +0100

    bug empty array in _dec_int32?

--- lib/BSON/Javascript.pm
+++ lib/BSON/Javascript.pm
@@ -5,11 +5,14 @@ class BSON::Javascript {
   has Str $.javascript;
   has Hash $.scope;
   
+  has Bool $.has_scope = False;
+  
   submethod BUILD ( Str :$javascript, Hash :$scope) {
   
-      # Store the attribute values.
+      # Store the attribute values. ? sets True if defined and filled.
       #
-      $!javascript = $javascript // '';
+      $!javascript = ?$javascript ?? $javascript !! 'function() {}';
       $!scope = $scope;
+      $!has_scope = ?$!scope;
   }
 }
