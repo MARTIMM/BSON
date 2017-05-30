19bf627c255cc4dce0a5bc283c63b66f8598ff82
Marcel Timmerman <mt1957@gmail.com>
2017-05-30 13:15:18 +0200

    test using decNumber library

---
+++ xt/d128-v1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DECNUMDIGITS 34            // work with up to 34 digits
+#include "decNumber.h"             // base number library
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define byte unsigned char
+
+//-----------------------------------------------------------------------------
+void printByteBin( byte b ) {
+  printf( BYTE_TO_BINARY_PATTERN,
+    (b & 0x80 ? '1' : '0'),    (b & 0x40 ? '1' : '0'),
+    (b & 0x20 ? '1' : '0'),    (b & 0x10 ? '1' : '0'),
+    (b & 0x08 ? '1' : '0'),    (b & 0x04 ? '1' : '0'),
+    (b & 0x02 ? '1' : '0'),    (b & 0x01 ? '1' : '0')
+  );
+  printf(" ");
+}
+
+//-----------------------------------------------------------------------------
+void showD128Pattern( char *x ) {
+
+  decContext set;
+  decContextDefault( &set, DEC_INIT_BASE); // initialize
+  set.traps = 0;
+  set.digits=DECNUMDIGITS;
+
+
+  union bits {
+    decNumber d128;
+    byte b128[16];
+  } v;
+
+  decNumberFromString( &v.d128, x, &set);
+
+
+  printf( "N = %s\n  ", x);
+  for( int i=0; i<16; i++) {
+    printf( " 0x%02x,", v.b128[i]);
+    if( !((i+1) % 8) ) { printf("\n  "); }
+  }
+  printf( "\n");
+
+/*
+  for( int i=0; i<16; i++) {
+    printByteBin(v.b128[i]);
+    if( !((i+1) % 8) ) { printf("\n"); }
+  }
+
+  printf( "\n");
+*/
+}
+
+//-----------------------------------------------------------------------------
+int main( int argc, char *args[]) {
+
+  for( int i = 1; i < argc; i++) {
+    showD128Pattern(args[i]);
+  }
+}
